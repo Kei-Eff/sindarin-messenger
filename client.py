4f230269dc44c66aff850f7bc5b0816cf8028ddd
Kei-Eff <karl.alberto@hotmail.com>
2021-09-29 23:50:48 +1000

    Fixed incorrect Docstrings

--- client.py
+++ client.py
@@ -3,26 +3,14 @@ import os
 
 
 class TranslationClient:
-    """Creates socket and gets IP address of the server.
-
-    Args:
-
-
-    Returns:
-        Function returns the translated text.
+    """Client that connects to the server and sends a message for translation.
     """
     def __init__(self):
         self.client_socket = None
         self.connect_to_server()
 
     def connect_to_server(self):
-        """Client that connects to the server and sends a message for translation.
-
-        Args:
-            message: The message to be sent to the server.
-
-        Returns:
-            The translated message from the server.
+        """Creates socket and gets IP address of the server.
         """
 
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -36,19 +24,19 @@ class TranslationClient:
         """Function that returns the user's input. Takes in no arguments.
 
         Returns:
-            message_to_send: The user's input.
+            The user's input.
         """
 
         message_to_send = input(">> ")
         return message_to_send
 
-    def send_message_to_translate(self, message_to_send):
-        """Client socket sends a message to the server socket.
+    def send_message_to_translate(self, message):
+        """Function that sends a message via the client socket.
 
         Args:
-            message_to_send (string): Sends the encoded message to the server.
+            message (string): The message to be sent.
         """
-        self.client_socket.sendall(str(message_to_send).encode('utf-8'))
+        self.client_socket.sendall(str(message).encode('utf-8'))
 
     def receive_translation(self):
         """Client connects to the server. Sends a message and waits for a response.
@@ -62,34 +50,32 @@ class TranslationClient:
 
     def disconnect(self):
         """Function closes client socket.
-
         """
         self.client_socket.close()
 
-# Create a TranslationClient object.
-translation_client = TranslationClient()
 
-while True:
+def main():
     """Main program loop that asks for user input, sends input to the server.
     Receives and prints translated message from the server.
     Loop will run until the user inputs '/exit' or reaches API request limit.
-
-    Args:
-        user_input: The text that the user wants to translate.
-
-    Returns:
-        The translated message.
     """
-    user_input = translation_client.get_input()
-    translation_client.send_message_to_translate(user_input)
 
-    if user_input == "/exit":
-        break
+    # Create a TranslationClient object.
+    translation_client = TranslationClient()
 
-    translated_output = translation_client.receive_translation()
+    while True:
+        user_input = translation_client.get_input()
+        translation_client.send_message_to_translate(user_input)
 
-    print(translated_output)
-    if translated_output.startswith("Too Many Requests"):
-        break
+        if user_input == "/exit":
+            break
 
-translation_client.disconnect()
+        translated_output = translation_client.receive_translation()
+
+        print(translated_output)
+        if translated_output.startswith("Too Many Requests"):
+            break
+
+    translation_client.disconnect()
+
+main()
